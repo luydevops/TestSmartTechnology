45dbe29b1d4859c520f3277982ed2967d9119ebb
luydevops <luydevops@example.com>
2025-03-15 08:51:06 -0500

    edit scriptshell and env

--- despliegue.sh
+++ despliegue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Directorio base
+# Directorio base (asumiendo que estás en el directorio testSmartTechnology)
 BASE_DIR="./"
 
 # URLs de los repositorios
@@ -16,3 +16,30 @@ git clone "$BACKEND_REPO" "$BASE_DIR/backend"
 git clone "$FRONTEND_REPO" "$BASE_DIR/frontend"
 
 echo "Repositorios descargados con éxito."
+
+# Construir los contenedores
+docker compose build
+
+# Verificar si la construcción fue exitosa
+if [ $? -eq 0 ]; then
+  echo "Construcción de contenedores exitosa."
+
+  # Desplegar los contenedores
+  docker compose up -d
+
+  # Verificar si el despliegue fue exitoso
+  if [ $? -eq 0 ]; then
+    echo "Despliegue de contenedores exitoso."
+
+    # Validar las rutas
+    if curl -s --head --fail http://localhost:8010 > /dev/null && curl -s --head --fail http://localhost:3010 > /dev/null; then
+      echo "Repositorios descargados y desplegados con éxito."
+    else
+      echo "Error: Una o ambas rutas no responden."
+    fi
+  else
+    echo "Error: Fallo en el despliegue de contenedores."
+  fi
+else
+  echo "Error: Fallo en la construcción de contenedores."
+fi
